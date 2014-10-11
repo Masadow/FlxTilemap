95e90b07e7d07607033fa1fe4351df30d94004eb
Tiago Ling Alexandre <ling.tiago@gmail.com>
2014-10-11 11:52:10 -0300

    - Removed export folder and added it to .gitignore.

--- export/flash/obj/EmbeddedAssets.hx
+++
@@ -1,7 +0,0 @@
-@:keep @:bitmap('assets/images/char_3.png') class __ASSET__images_char_3_png extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
-@:keep @:bitmap('assets/images/char_4.png') class __ASSET__images_char_4_png extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
-@:keep @:file('assets/images/images-go-here.txt') class __ASSET__images_images_go_here_txt extends flash.utils.ByteArray { }
-@:keep @:bitmap('assets/images/tileset.png') class __ASSET__images_tileset_png extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
-@:keep @:file('assets/data/data-goes-here.txt') class __ASSET__data_data_goes_here_txt extends flash.utils.ByteArray { }
-@:keep @:sound('C:/GitRepos/flixel/assets/sounds/beep.mp3') class __ASSET__assets_sounds_beep_mp3 extends flash.media.Sound { }
-@:keep @:sound('C:/GitRepos/flixel/assets/sounds/flixel.mp3') class __ASSET__assets_sounds_flixel_mp3 extends flash.media.Sound { }
